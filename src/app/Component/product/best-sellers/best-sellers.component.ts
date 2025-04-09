67e8dac051925cf9dd83da512fbaecb671ec579d
AsnanAshraf <asnan.ashraf@codecones.com>
2025-04-09 23:42:55 +0500

    add to cart functionality in the best selling component

--- src/app/Component/product/best-sellers/best-sellers.component.ts
+++ src/app/Component/product/best-sellers/best-sellers.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../../Services/product/product.service';
-import { Plant, ProductApiResponse } from '../../../Interfaces/productApi';
+import { CartItem, Plant, ProductApiResponse } from '../../../Interfaces/productApi';
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Router } from '@angular/router';
+import { CartService } from '../../../Services/Cart/cart.service';
+import { CartDrawerComponent } from "../../../Shared/cart-drawer/cart-drawer.component";
 
 @Component({
   selector: 'app-best-sellers',
   standalone: true,
-  imports: [NgIf, NgFor, NgClass],
+  imports: [NgIf, NgFor, NgClass, CartDrawerComponent],
   templateUrl: './best-sellers.component.html',
   styleUrl: './best-sellers.component.scss'
 })
 export class BestSellersComponent implements OnInit {
   products: Plant[] = [];
+  visible: boolean = false;
+  cartItems: CartItem[] = [];
 
-  constructor(private productService: ProductService, private router: Router) {}
+  constructor(private productService: ProductService, private router: Router, private cartService: CartService) {}
 
   ngOnInit() {
     this.fetchProducts();
@@ -34,5 +38,16 @@ export class BestSellersComponent implements OnInit {
   getProductDescription(id: any){
     const productId = Number(id)
     this.router.navigate([`product/${productId}`]);
+  }
+
+  addToCart(product: Plant) {
+    this.cartService.addItem({
+      id: product.id,
+      name: product.name,
+      selling_price: product.selling_price,
+      img: product.img,
+      quantity: 1
+    });
+    // No need to manually set visible - the service handles it
   }
 }
